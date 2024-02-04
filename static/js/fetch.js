d00fa6e92f3eea02b1b27ec762141696c0993010
salimou <bambo.diaby@hotmail.fr>
2024-02-04 16:29:58 +0100

    Init

---
+++ static/js/fetch.js
@@ -0,0 +1,31 @@
+﻿
+async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+        method: "POST",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+function sendRegistrationLink() {
+    var editionId = document.getElementById('edition').value;
+    var communeId = document.getElementById('commune').value;
+    var lastName = document.getElementById('name').value;
+    var firstName = document.getElementById('firstname').value;
+    var birthday = document.getElementById('birthday').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var isFibb = document.getElementById('licenciefibb').checked;
+
+    console.log({ editionId, commune, lastName, firstName, birthday, email, phone, isFibb })
+
+    postData(`/register-team/${editionId}`, { editionId, communeId, lastName, firstName, birthday, email, phone, isFibb })
+        .then(() => {
+
+        }).catch((error) => { console.log({error}); })
+}
---
+++ static/js/site.js
@@ -0,0 +1,40 @@
+﻿
+var jsFunctions = {};
+
+jsFunctions.setCurrentTournoisCommune = (communeId) => {
+    DotNet.invokeMethodAsync("IvoirBballNet", "SetCurrentTournoisCommune", communeId).then(() => {
+        console.log('Current commune set to ' + communeId)
+    }).catch(ex => console.log(ex))
+}
+
+jsFunctions.callOpenEditCommuneTournois = (dotnNetObjRef, communeId) => {
+    dotnNetObjRef.invokeMethodAsync("SetCurrentTournoisCommune", communeId)
+    var el = document.getElementById("sideNav")
+    el.style.width = "80%";
+    el.style.position = "fixed";
+}
+
+jsFunctions.callCloseEditCommuneTournois = (dotNetObjRef) => {
+    dotNetObjRef.invokeMethodAsync("ClearCurrentTournoisCommune", null)
+    document.getElementById("sideNav").style.width = "0";
+}
+
+function openCommuneTeamsNav(communeId) {
+   
+    jsFunctions.setCurrentTournoisCommune(communeId)
+}
+
+function closeCommuneTeamsNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
+function openNav() {
+    var el = document.getElementById("sideNav")
+    el.style.width = "80%";
+    el.style.position = "fixed";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
